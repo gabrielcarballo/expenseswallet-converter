139c26846b08aa7ce7e260dc7a10b67ac091d308
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-11-15 18:23:33 -0300

    Requisite 7 done

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,24 +3,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Table extends Component {
-  logExpenses = () => {
-    const { expenses } = this.props;
-    console.log(expenses);
-  };
-
   render() {
-    /* const { expenses } = this.props; */
+    const { expenses } = this.props;
     return (
       <table>
-        <th onClick={ this.logExpenses }>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((e, i) => (
+            <tr key={ i }>
+              <td>{e.description}</td>
+              <td>{e.tag}</td>
+              <td>{e.method}</td>
+              <td>{Number(e.value).toFixed(2)}</td>
+              <td>{e.exchangeRates[e.currency].name}</td>
+              <td>{Number(e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+              <td>
+                {(Number(e.value) * Number(e.exchangeRates[e.currency].ask)).toFixed(2)}
+              </td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
